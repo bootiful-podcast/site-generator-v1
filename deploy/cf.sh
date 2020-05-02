ce3a56e1296f5b03ccd0a5df15afa38d6589797f
Josh Long <josh@joshlong.com>
2020-05-02 02:54:22 -0700

    up

--- deploy/cf.sh
+++ deploy/cf.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+source "$(cd $(dirname $0) && pwd)/env.sh"
+
 
 mkdir -p $HOME/bin
-cp ./deploy/cf-linux $HOME/bin/cf
+cp ./deploy/cf-6.23.1 $HOME/bin/cf
 cf login  -u $CF_USER -a $CF_API -o $CF_ORG -p $CF_PASSWORD -s $CF_SPACE
+cf install-plugin -f ./deploy/scheduler-for-pcf-cliplugin-linux64-binary-1.1.0
 
 
 
--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,11 +1,32 @@
 #!/usr/bin/env bash
 
-export APP_NAME=site-generator
-export TASK_NAME=${APP_NAME}-task
-cf push --no-start -u none --no-route -p target/${APP_NAME}.jar ${APP_NAME}
-cf set-env ${APP_NAME} GIT_PASSWORD $GIT_PASSWORD
-cf set-env ${APP_NAME} GIT_USERNAME $GIT_USERNAME
-cf set-env ${APP_NAME} SPRING_PROFILES_ACTIVE cloud
-cf set-env ${APP_NAME} GIT_URI $GIT_URI
-cf set-env ${APP_NAME} PODCAST_RMQ_ADDRESS $PODCAST_RMQ_ADDRESS
-cf start $APP_NAME
+source "$(cd $(dirname $0) && pwd)/env.sh"
+
+APP_NAME=twitter-ingest-job
+JOB_NAME=${APP_NAME}
+SCHEDULER_SERVICE_NAME=scheduler-joshlong
+REDIS_NAME=redis-cache
+
+
+cf d -f ${APP_NAME} 
+cf push -b java_buildpack -u none --no-route --no-start -p target/${APP_NAME}.jar ${APP_NAME}
+cf set-health-check $APP_NAME none
+
+cf s | grep ${SCHEDULER_SERVICE_NAME} || cf cs scheduler-for-pcf standard ${SCHEDULER_SERVICE_NAME}
+cf bs ${APP_NAME} ${SCHEDULER_SERVICE_NAME}
+
+cf s | grep ${REDIS_NAME} || cf cs rediscloud 100mb ${REDIS_NAME}
+cf bs ${APP_NAME} ${REDIS_NAME}
+
+cf set-env ${APP_NAME} PINBOARD_TOKEN "${PINBOARD_TOKEN}"
+cf set-env ${APP_NAME} TWITTER_TWI_CLIENT_KEY ${TWITTER_TWI_CLIENT_KEY}
+cf set-env ${APP_NAME} TWITTER_TWI_CLIENT_KEY_SECRET ${TWITTER_TWI_CLIENT_KEY_SECRET}
+cf set-env ${APP_NAME} JBP_CONFIG_OPEN_JDK_JRE '{ jre: { version: 11.+}}'
+
+cf restart ${APP_NAME}
+
+cf jobs  | grep $JOB_NAME && cf delete-job -f ${JOB_NAME}
+cf create-job ${APP_NAME} ${JOB_NAME} ".java-buildpack/open_jdk_jre/bin/java org.springframework.boot.loader.JarLauncher"
+cf schedule-job ${JOB_NAME} "*/15 * ? * *"
+
+cf run-job ${JOB_NAME}
